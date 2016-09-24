880f0f01b4281308dcb866437bc4fdb98cf82737
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-09-24 15:42:37 +0200

    shortened code, and made it more straight-forward

--- src/main/scala/com/github/jancajthaml/ShardHash.scala
+++ src/main/scala/com/github/jancajthaml/ShardHash.scala
@@ -2,30 +2,20 @@ package com.github.jancajthaml
 
 object shardHash extends ((String, Int) => String) {
 
-  def apply(x: String, modulo: Int): String = {
+  def apply(x: String, mod: Int): String = {
     var hash: Int = 0
-    val limit: Int = ((Int.MaxValue / 16) * 0.8).asInstanceOf[Int]
+    val limit: Int = ((Int.MaxValue >>> 4) * .8).asInstanceOf[Int]
 
+    //@info remove mutation and introduce recursion
     x.getBytes.foreach { b => {
-      //@info this should be part or private object x in scala here
-      val digit: Int = com.github.jancajthaml.Latin.A(b.toChar)
+      hash = (hash << 4) + (if (b <= 57) (b + 16 & 0x1F) else ((b + 31 & 0x1F) + 10))
 
-      hash *= 16
-
-      if ((digit & 0x1F) == 9) {
-        val codePoint = b.toInt + ((digit & 0x3E0) >> 5) & 0x1F
-        hash = if (codePoint < 16) hash + codePoint else hash - 1
-      } else if ((digit & 0xC00) == 0x00000C00) {
-        val codePoint = b.toInt + ((digit & 0x3E0) >> 5) & 0x1F
-        hash = if (codePoint < 26) hash + codePoint + 10 else hash - 1
-      }
-
-      if (hash >= limit) {
-        hash %= modulo 
+      if (hash > limit) {
+        hash %= mod 
       }
     } }
 
-    (if (hash >= modulo) (hash % modulo) else hash).toString
+    (if (hash >= mod) (hash % mod) else hash).toString
   }
 
 }

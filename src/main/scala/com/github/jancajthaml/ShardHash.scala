718dd73c7139c07eaf9399ee7be4bef7aafdb448
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-09-24 16:07:10 +0200

    improved performance slightly

--- src/main/scala/com/github/jancajthaml/ShardHash.scala
+++ src/main/scala/com/github/jancajthaml/ShardHash.scala
@@ -4,15 +4,12 @@ object shardHash extends ((String, Int) => String) {
 
   def apply(x: String, mod: Int): String = {
     var hash: Int = 0
-    val limit: Int = ((Int.MaxValue >>> 4) * .8).asInstanceOf[Int]
-
+    //@info we assume that reasonable threshold is 40:60 instead of median
+    val limit: Int = ((Int.MaxValue >>> 5) * .4 + (Int.MaxValue >>> 4) * .6).asInstanceOf[Int]
     //@info remove mutation and introduce recursion
     x.getBytes.foreach { b => {
-      hash = (hash << 4) + (if (b <= 57) (b + 16 & 0x1F) else ((b + 31 & 0x1F) + 10))
-
-      if (hash > limit) {
-        hash %= mod 
-      }
+      hash = ((hash << 4) + (if (b < 58) (b + 16 & 0x1f) else ((b + 31 & 0x1f) + 10)))
+      if (hash > limit) hash %= mod
     } }
 
     (if (hash >= mod) (hash % mod) else hash).toString

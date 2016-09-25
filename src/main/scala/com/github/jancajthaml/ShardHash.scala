c67878d116969846f56da2d377b08939755fe56d
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-09-25 11:50:30 +0200

    using ASCII charset

--- src/main/scala/com/github/jancajthaml/ShardHash.scala
+++ src/main/scala/com/github/jancajthaml/ShardHash.scala
@@ -7,8 +7,7 @@ object shardHash extends ((String, Int) => String) {
     //@info we assume that reasonable threshold is 40:60 instead of median
     val limit: Int = ((Int.MaxValue >>> 5) * .4 + (Int.MaxValue >>> 4) * .6).asInstanceOf[Int]
     //@info remove mutation and introduce recursion
-    //@info ISO-8859-1 is Latin1 charset where mapping of numbers and integers can be assumed
-    x.getBytes("ISO-8859-1").foreach { b => {
+    x.getBytes("US-ASCII").foreach { b => {
       hash = ((hash << 4) + (if (b < 58) (b + 16 & 0x1f) else ((b + 31 & 0x1f) + 10)))
       if (hash > limit) hash %= mod
     } }
